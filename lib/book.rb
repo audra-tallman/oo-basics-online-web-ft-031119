9aefe00fcf74f9e2dc39276879f16866146098a7
Audra Tallman <audra.tallman@hotmail.com>
2019-03-19 19:49:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,17 +10,25 @@ class Book
   def author=(author)   #setter method
     @author = author
   
-  def author
+  def author  #getter method
     @author
   end
   
-  def page_count=(num)
+  def page_count=(num)  #setter method
     @page_count = num
   end 
   
-  def page_count
+  def page_count  #getter method
     @page_count
   end
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def genre
+    @genre
+  end 
   
 end
 
